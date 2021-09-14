8b56cf49db74fd3513fc57183d709beb379da3f7
Moises Gaioli Estevão <moi.rp@hotmail.com>
2021-09-14 17:45:19 -0300

    Finalizado

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Characters from './components/Characters';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [characterList, setCharacterList] = useState([]);
+
+  useEffect (() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+    .then(response => response.json())
+    .then(response => setCharacterList(response.results))
+    .catch(error => console.log(error));
+  }, [])
+
+  console.log(characterList)
+
+  return (
+      <main className="main-container">
+
+        <Characters characterList={characterList} />
+
+      </main>
+  );
+}
+
+export default App;
